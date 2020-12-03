7082b411c931540f78a0f7fd21295a18f660dc83
dajensen <dajensen@users.noreply.github.com>
2020-12-02 22:22:19 -0700

    Sound playback MUCH IMPROVED!
    This is kind of dumb, because I copied all of the samples over into the test directory, but I just want to check this in right now... it was hard to find the one thing that had to change.  Biggest factor was NOT resetting the sample rate when loading a new sound sample.

--- SoundTest/SoundTest.ino
+++ SoundTest/SoundTest.ino
@@ -1,28 +1,11 @@
-#include "wifi_config.h"
-#include <WiFi.h>
-#include <ezTime.h>
 #include "WavPlayer.h"
 #include "sound/WavData.h"
 
 WavPlayer player;
-WiFiClient wifiClient;
-String uniqueId;
-Timezone LocalTime;
-#define LOCAL_TIMEZONE "America/Denver"
-
-int time_hour;
-int time_minute;
 
 const int STATUS_LED = 10;
 const int BUTTON_PIN = 37;
 const int SPEAK_TIMEOUT = 5000;
-
-int touchThreshold = 60; // copper plate sensitivity level
-
-// TODO: 
-// update IDE... need spiffs
-// look at the DAC audio guy's article
-// SPIFFS: ESP32 ... easy to upload all the .wav files this way.
 
 enum SpeechState {
   WAITING,
@@ -32,125 +15,37 @@ enum SpeechState {
 
 SpeechState speechstate;
 
-void PrintMacAddr(const unsigned char *mac) {
-  Serial.print("MAC: ");
-  Serial.print(mac[5],HEX);
-  Serial.print(":");
-  Serial.print(mac[4],HEX);
-  Serial.print(":");
-  Serial.print(mac[3],HEX);
-  Serial.print(":");
-  Serial.print(mac[2],HEX);
-  Serial.print(":");
-  Serial.print(mac[1],HEX);
-  Serial.print(":");
-  Serial.println(mac[0],HEX);
-}
+const int NUM_TEST_WAVS = sizeof(testWav) / sizeof(testWav[0]);
 
-// SETUP and LOOP are the two required functions in Arduino
 
 void setup() {
   digitalWrite(STATUS_LED, 1);    // Inverted for the built-in light
   pinMode(BUTTON_PIN, INPUT_PULLUP);
   pinMode(STATUS_LED, OUTPUT);
-    
+
+  while (! Serial) {
+    delay(1);
+  }
+
   Serial.begin(115200);
-  delay(5000); // This is necessary for the ESP32 to get serial messages
   Serial.println("..... STARTING .....");
-  WiFi.begin(cfg.ssid, cfg.password);
-  while (WiFi.status() != WL_CONNECTED) {
-		delay(100);
-	}
-  Serial.println("Wifi Connected");
- 
-  waitForSync();  // from ezTime, means wait for NTP to get date and time.
-  Serial.println("synced");
-  
-  LocalTime.setLocation(LOCAL_TIMEZONE);
 
   speechstate = WAITING;
   Serial.println("Done with setup()");
 }
 
 void loop() {
+  static int file_num = -1;
+  static uint32_t start_time = 0;
 
-  switch (speechstate)
-  {
-  case SPEAK_HOUR:
-    speechstate = SpeakHour();
-    break;
-  case SPEAK_MINUTE:
-    speechstate = SpeakMinute();
-    break;
-  case WAITING:
-  default:
-    speechstate = DoWaiting();
-    break;
-  }
-} 
-
-SpeechState SpeakHour() {
-  SpeechState rv = SPEAK_HOUR;
-
-  if (!player.Update()) { 
-    // this handles the change from hour to minute. No longer playing the hour.
-    Serial.println("minute " + (String)time_minute);
-    player.StartPlaying(minuteWav[time_minute]);
-    Serial.println("HOUR->MINUTE");
-    rv = SPEAK_MINUTE;
-  }
-  return rv;
-}
-
-SpeechState SpeakMinute() {
-  SpeechState rv = SPEAK_MINUTE;
-
-    if (!player.Update()) {
-      rv = WAITING;
-      Serial.println("MINUTE->WAITING");
-    }
-    return rv;
-}
-
-bool IsTriggered() {
-  bool rv = false;
-  uint32_t currTime = millis();
-  static uint32_t prevTime = 0;
-
-  static int touchFrames;
-  int touchTriggerFrames = 10;
-  
-  if(touchRead(T0) < touchThreshold) {
-    touchFrames++;
-  } else {
-    touchFrames = 0;
-  }
-  
-  if(touchFrames > touchTriggerFrames){ 
-    if(currTime - prevTime >= SPEAK_TIMEOUT) {
-      prevTime = currTime;
-      rv = true;
-    }
-  }
-  return rv;
-}
-
-SpeechState DoWaiting() {
-  SpeechState rv = WAITING;
-  
-  if(IsTriggered()) {
-
-    // this handles the change from hour to minute. We are done playing the hour.  Now changing states to the SPEAK_MINUTE state.
-    time_hour = LocalTime.hour() % 12;
-    time_minute = LocalTime.minute() / 5 * 5;
-    Serial.println("Got time: hour " + (String)time_hour + " and minute " + (String)time_minute);
+  if(!player.Update()) {
+    file_num++;
+    if(start_time > 0 && file_num < NUM_TEST_WAVS + 1)
+      Serial.println("Used time: " + (String)(millis() - start_time));
     
-    Serial.println("hour " + (String)time_hour);
-    player.StartPlaying(hourWav[time_hour]);
-    Serial.println("WAITING->HOUR");
-    rv = SPEAK_HOUR;
+    if(file_num < NUM_TEST_WAVS) {
+      start_time = millis();
+      player.StartPlaying(testWav[file_num]);
+    }
   }
-//  Serial.println(touchRead(T0));
-  events();
-  return rv;
 }
---
+++ SoundTest/WavPlayer.h
@@ -0,0 +1,31 @@
+#include <Arduino.h>
+#include "driver/i2s.h"                       // Library of I2S routines, comes with ESP32 standard install
+#include "WavHeader.h"
+
+// I2S physical wiring connection pins
+const int BCK_IO_PIN = 27;
+const int WS_IO_PIN = 26; // 33 has also worked, but I started getting a lot of weird crashes;
+const int DATA_OUT_PIN = 25;
+
+static const i2s_port_t i2s_num = I2S_NUM_0;  // i2s port number
+
+
+class WavPlayer {
+public:
+    WavPlayer();
+    bool StartPlaying(const unsigned char *data);
+    bool Update();
+    bool DonePlaying();
+
+protected:
+    WavHeader header;
+    unsigned const char* TheData;
+    uint32_t DataIdx=0;
+    bool isValid;
+    uint32_t play_start_time;
+    uint32_t play_end_time;
+
+    void DumpWavHeader(WavHeader* Wav);
+    bool ValidWavData(WavHeader* Wav);
+    void PrintData(const char* Data,uint8_t NumBytes);
+};
