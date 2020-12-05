a801cc3493315bb37f927ec7c70f722ad6337bc4
Ryan Jensen <rjensen96+github@gmail.com>
2020-12-05 09:34:51 -0700

    Deleted Excess .wav and .h for sound
    Deleted files that we never plan to use. Will only speak multiples of 5 to save space.

--- Sound-Clock/sound/WavData.h
+++ Sound-Clock/sound/WavData.h
@@ -19,60 +19,67 @@
 #include "10.h"
 #include "11.h"
 #include "12.h"
-#include "13.h"
-#include "14.h"
+// #include "13.h"
+// #include "14.h"
 #include "15.h"
-#include "16.h"
-#include "17.h"
-#include "18.h"
-#include "19.h"
+// #include "16.h"
+// #include "17.h"
+// #include "18.h"
+// #include "19.h"
 #include "20.h"
-#include "21.h"
-#include "22.h"
-#include "23.h"
-#include "24.h"
+// #include "21.h"
+// #include "22.h"
+// #include "23.h"
+// #include "24.h"
 #include "25.h"
 #include "26.h"
-#include "27.h"
-#include "28.h"
-#include "29.h"
+// #include "27.h"
+// #include "28.h"
+// #include "29.h"
 #include "30.h"
-#include "31.h"
-#include "32.h"
-#include "33.h"
-#include "34.h"
+// #include "31.h"
+// #include "32.h"
+// #include "33.h"
+// #include "34.h"
 #include "35.h"
-#include "36.h"
-#include "37.h"
-#include "38.h"
-#include "39.h"
+// #include "36.h"
+// #include "37.h"
+// #include "38.h"
+// #include "39.h"
 #include "40.h"
-#include "41.h"
-#include "42.h"
-#include "43.h"
-#include "44.h"
+// #include "41.h"
+// #include "42.h"
+// #include "43.h"
+// #include "44.h"
 #include "45.h"
-#include "46.h"
-#include "47.h"
-#include "48.h"
-#include "49.h"
+// #include "46.h"
+// #include "47.h"
+// #include "48.h"
+// #include "49.h"
 #include "50.h"
-#include "51.h"
-#include "52.h"
-#include "53.h"
-#include "54.h"
+// #include "51.h"
+// #include "52.h"
+// #include "53.h"
+// #include "54.h"
 #include "55.h"
-#include "56.h"
-#include "57.h"
-#include "58.h"
-#include "59.h"
+// #include "56.h"
+// #include "57.h"
+// #include "58.h"
+// #include "59.h"
 
 const unsigned char *hourWav[] = {__12_wav, __1_wav, __2_wav, __3_wav, __4_wav, __5_wav, __6_wav, __7_wav, __8_wav, __9_wav, __10_wav,__11_wav,__12_wav};
+
 const unsigned char *minuteWav[] = {
-	__01_wav, __01_wav, __02_wav, __03_wav, __04_wav, __05_wav, __06_wav, __07_wav, __08_wav, __09_wav, 
-	__10_wav, __11_wav, __12_wav, __13_wav, __14_wav, __15_wav, __16_wav, __17_wav, __18_wav, __19_wav, 
-	__20_wav, __21_wav, __22_wav, __23_wav, __24_wav, __25_wav, __26_wav, __27_wav, __28_wav, __29_wav, 
-	__30_wav, __31_wav, __32_wav, __33_wav, __34_wav, __35_wav, __36_wav, __37_wav, __38_wav, __39_wav, 
-	__40_wav, __41_wav, __42_wav, __43_wav, __44_wav, __45_wav, __46_wav, __47_wav, __48_wav, __49_wav, 
-	__50_wav, __51_wav, __52_wav, __53_wav, __54_wav, __55_wav, __56_wav, __57_wav, __58_wav, __59_wav
+	__01_wav, __05_wav, __10_wav, __15_wav, __20_wav, __25_wav, 
+	__30_wav, __35_wav, __40_wav, __45_wav, __50_wav, __55_wav
 };
+
+// the original, with all of the .wav files included
+// const unsigned char *minuteWav[] = {
+// 	__01_wav, __01_wav, __02_wav, __03_wav, __04_wav, __05_wav, __06_wav, __07_wav, __08_wav, __09_wav, 
+// 	__10_wav, __11_wav, __12_wav, __13_wav, __14_wav, __15_wav, __16_wav, __17_wav, __18_wav, __19_wav, 
+// 	__20_wav, __21_wav, __22_wav, __23_wav, __24_wav, __25_wav, __26_wav, __27_wav, __28_wav, __29_wav, 
+// 	__30_wav, __31_wav, __32_wav, __33_wav, __34_wav, __35_wav, __36_wav, __37_wav, __38_wav, __39_wav, 
+// 	__40_wav, __41_wav, __42_wav, __43_wav, __44_wav, __45_wav, __46_wav, __47_wav, __48_wav, __49_wav, 
+// 	__50_wav, __51_wav, __52_wav, __53_wav, __54_wav, __55_wav, __56_wav, __57_wav, __58_wav, __59_wav
+// };
